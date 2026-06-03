5fdbf591867538442ac9fbd1ca2e52ad31af63eb
Pinbak <23728566+Pinbak@users.noreply.github.com>
2026-06-03 12:02:04 +0100

    Get world space when clicked

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+
+
+    private void Start()
+    {
+        inputManager.OnMouseDown += HandleMouseClick;
+    }
+
+    private void HandleMouseClick(Vector3Int position)
+    {
+        Debug.Log(position);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,6 +2,8 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class InputManager : MonoBehaviour
 {
@@ -9,17 +11,25 @@ public class InputManager : MonoBehaviour
     public Action<Vector3Int> OnMouseHold { get; set; }
     public Action OnMouseUp { get; set; }
 
+    [SerializeField] private InputActionReference leftMouseButton;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask ground;
     private Vector2 _cameraMovementVector;
 
-    private void Update()
+    private void OnEnable()
     {
-        IsPointerDown();
-        IsPointerUp();
-        IsClickHold();
-        GetInputs();
+        leftMouseButton.action.started += IsPointerDown;
+        leftMouseButton.action.performed += IsPointerHold;
+        leftMouseButton.action.canceled += IsPointerUp;
+        leftMouseButton.action.Enable();
+    }
 
+    private void OnDisable()
+    {
+        leftMouseButton.action.started -= IsPointerDown;
+        leftMouseButton.action.performed -= IsPointerHold;
+        leftMouseButton.action.canceled -= IsPointerUp;
+        leftMouseButton.action.Disable();
     }
 
     private void GetInputs()
@@ -27,36 +37,36 @@ public class InputManager : MonoBehaviour
         _cameraMovementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
     
+    private void IsPointerHold(InputAction.CallbackContext _)
+    {
+        // if (EventSystem.current.IsPointerOverGameObject()) return;
+        var position = RaycastGround();
+        if (position.HasValue)
+            OnMouseHold?.Invoke(position.Value);
+    }
+
+    private void IsPointerUp(InputAction.CallbackContext _)
+    {
+        // if (EventSystem.current.IsPointerOverGameObject()) return;
+        OnMouseUp?.Invoke();
+    }
+
+    private void IsPointerDown(InputAction.CallbackContext _)
+    {
+        // if (EventSystem.current.IsPointerOverGameObject()) return;
+        var position = RaycastGround();
+        if (position.HasValue)
+            OnMouseDown?.Invoke(position.Value);
+    }
+    
     /// <summary>
     ///     Get position where the mouse touches the ground layer
     /// </summary>
     private Vector3Int? RaycastGround()
     {
-        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        var ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
         if (Physics.Raycast(ray, out var hit, Mathf.Infinity, ground))
             return Vector3Int.RoundToInt(hit.point);
         return null;
-    }
-    
-    private void IsClickHold()
-    {
-        if (!Input.GetMouseButton(0) || EventSystem.current.IsPointerOverGameObject()) return;
-        var position = RaycastGround();
-        if (position is not null)
-            OnMouseHold?.Invoke(position.Value);
-    }
-
-    private void IsPointerUp()
-    {
-        if (!Input.GetMouseButtonUp(0) || EventSystem.current.IsPointerOverGameObject()) return;
-        OnMouseUp?.Invoke();
-    }
-
-    private void IsPointerDown()
-    {
-        if (!Input.GetMouseButtonDown(0) || EventSystem.current.IsPointerOverGameObject()) return;
-        var position = RaycastGround();
-        if (position is not null)
-            OnMouseDown?.Invoke(position.Value);
     }
 }
