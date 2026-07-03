d11944178425bb79a96b776cad096b1f4f97bd97
Pinbak <23728566+Pinbak@users.noreply.github.com>
2026-07-03 19:34:05 +0100

    Testing with a bunch more houses

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -31,6 +31,26 @@ public class BuildingManager : MonoBehaviour
         TestHouse2 = Instantiate(residentialLowWealthPrefab, new Vector3(0f, 0f, -2f), Quaternion.identity, transform);
         TestHouse2.Init(this);
         placementManager.PlaceBuilding(TestHouse2.BuildingInformation);
+        
+        var testHouse3 = Instantiate(residentialLowWealthPrefab, new Vector3(-2f, 0f, 2f), Quaternion.identity, transform);
+        testHouse3.Init(this);
+        placementManager.PlaceBuilding(testHouse3.BuildingInformation);
+        
+        var testHouse4 = Instantiate(residentialLowWealthPrefab, new Vector3(-4f, 0f, 2f), Quaternion.identity, transform);
+        testHouse4.Init(this);
+        placementManager.PlaceBuilding(testHouse4.BuildingInformation);
+        
+        var testHouse5 = Instantiate(residentialLowWealthPrefab, new Vector3(-6f, 0f, 2f), Quaternion.identity, transform);
+        testHouse5.Init(this);
+        placementManager.PlaceBuilding(testHouse5.BuildingInformation);
+        
+        var testHouse6 = Instantiate(residentialLowWealthPrefab, new Vector3(-2f, 0f, -2f), Quaternion.identity, transform);
+        testHouse6.Init(this);
+        placementManager.PlaceBuilding(testHouse6.BuildingInformation);
+        
+        var testHouse7 = Instantiate(residentialLowWealthPrefab, new Vector3(-4f, 0f, -2f), Quaternion.identity, transform);
+        testHouse7.Init(this);
+        placementManager.PlaceBuilding(testHouse7.BuildingInformation);
 
         TestIndustrial = Instantiate(industrialLowWealthPrefab, new Vector3(2f, 0f, 2f), Quaternion.identity,
             transform);
